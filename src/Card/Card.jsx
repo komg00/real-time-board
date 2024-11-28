4b567a39b9076989671beea4624c626ed7cc07b1
komg00 <komg00@naver.com>
2024-11-28 15:30:36 +0900

    Feat: Add button for create room & join room

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Card.scss";
 
-const Card = ({ title, description, imageSrc, onClick }) => {
+const Card = ({ title, description, imageSrc, onClick, contents }) => {
   return (
-    <div className="card" onClick={onClick}>
+    <div className="card">
       <img src={imageSrc} alt={title} className="card-image" />
       <div className="card-contents">
         <h2 className="card-title">{title}</h2>
         <p className="card-description">{description}</p>
+        <button className="card-button" onClick={onClick}>
+          {contents}
+        </button>
       </div>
     </div>
   );
